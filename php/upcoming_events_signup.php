29143088237d0786a3ae6d8512f6b1c293988cc5
Sydney Blackburn <sydneyblackburn10@gmail.com>
2019-03-21 02:49:49 -0700

    Re-styled form on Events Signup page and updated CSS

--- php/upcoming_events_signup.php
+++ php/upcoming_events_signup.php
@@ -15,7 +15,7 @@ Fall 2018
         <title>SDSU NHM - Events Sign Up</title>
         <meta charset="utf-8" />
         <!-- CSS -->
-        <link rel="stylesheet" type="text/css" href="../css/style.css" />
+        <link rel="stylesheet" type="text/css" href="../css/style.css?v=1.1" />
         <!-- JS -->
         <script type="text/javascript" src="../js/total.js"></script>
     </head>
@@ -55,9 +55,8 @@ Fall 2018
         <div id="container">
             <h3>Sign up for the SDSU NHM's upcoming events!</h3>
             <hr />
-            <h4>Please fill out this form and click Submit when finished</h4>
-            <!-- <section class="upcomingEventsSignupSection">
-                <article class="upcomingEventsSignupArticle"> -->
+            <section class="upcomingEventsSignupSection">
+                <article class="upcomingEventsSignupArticle">
                 <?php
                     
                 // define variables and set to empty values
@@ -357,114 +356,141 @@ Fall 2018
                 }
                 ?>
                 
-                <form name="upcomingEventsSignUp" action = "<?php
-                    echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <h4>Please fill out this form and click Submit when finished</h4>
+                
+                <form id="upcomingEventsForm" name="upcomingEventsSignUp" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                     
-                    <!-- First name field -->
-                    <span class="required">*</span>
-                    <input type="text" name="firstname" id="firstname" size="40"  maxlength="60" value="<?php echo $firstname; ?>" placeholder="First Name"><span class="error"><?php echo " " . $firstnameErr;?></span>
-                    <br /><br />
+                    <table>
+                        <!-- First name field -->
+                        <tr>
+                            <td>First Name:<span class="required">*</span></td>
+                            <td>
+                                <input type="text" name="firstname" id="firstname" size="40"  maxlength="60" value="<?php echo $firstname; ?>"><span class="error"><?php echo " " . $firstnameErr;?></span>
+                            </td>
+                        </tr>
                         
-                    <!-- Last name field -->
-                    <span class="required">*</span>
-                    <input type="text" name="lastname" id="lastname" size="40"  maxlength="60" value="<?php echo $lastname; ?>" placeholder="Last Name"><span class="error"><?php echo " " . $lastnameErr;?></span>
-                    <br /><br />
-                    
-                    <!-- Email field -->
-                    <span class="required">*</span>
-                    <input type="email" name="email" id="email" size="40"  maxlength="60" value="<?php echo $email; ?>" placeholder="Email"><span class="error"><?php echo " " . $emailErr;?></span>
-                    <br /><br />
-                    
-                    <!-- Address1 field -->
-                    <span class="optional">&#160;</span>
-                    <input type="address1" name="address1" id="address1" size="40"  maxlength="60" value="<?php echo $address1; ?>" placeholder="Address Line 1"><span class="error"><?php echo " " . $address1Err;?></span>
-                    <br /><br />
-                    
-                    <!-- Address2 field -->
-                    <span class="optional">&#160;</span>
-                    <input type="address2" name="address2" id="address2" size="40"  maxlength="60" value="<?php echo $address2; ?>" placeholder="Address Line 2"><span class="error"><?php echo " " . $address2Err;?></span>
-                    <br /><br />
-                    
-                    <!-- City field -->
-                    <span class="optional">&#160;</span>
-                    <input type="city" name="city" id="city" size="25"  maxlength="30" value="<?php echo $city; ?>" placeholder="City"><span class="error"><?php echo " " . $cityErr;?></span>
-                    
-                    <!-- State field -->
-                    <span class="optional">&#160;</span>
-                    <input type="state" name="state" id="state" size="4"  maxlength="2" value="<?php echo $state; ?>" placeholder="State"><span class="error"><?php echo " " . $stateErr;?></span>
-                    
-                    <!-- Zip field -->
-                    <span class="optional">&#160;</span>
-                    <input type="zip" name="zip" id="zip" size="5"  maxlength="5" value="<?php echo $zip; ?>" placeholder="Zip"><span class="error"><?php echo " " . $zipErr;?></span>
-                    <br /><br />
-                    
-                    <!-- Country field -->
-                    <span class="optional">&#160;</span>
-                    <input type="country" name="country" id="country" size="40"  maxlength="60" value="<?php echo $country; ?>" placeholder="Country"><span class="error"><?php echo " " . $countryErr;?></span>
-                    <br /><br />
-                    
-                    <!-- Phone field -->
-                    <span class="optional">&#160;</span>
-                    <input type="phone" name="phone" id="phone" size="40"  maxlength="60" value="<?php echo $phone; ?>" placeholder="Phone Number, ex. 000-000-0000"><span class="error"><?php echo " " . $phoneErr;?></span>
-                    <br /><br />
-                    
-                    <!-- Event Field -->
-                    <span class="required">*</span>
-                    <select name="event" id="event">
-                        <option value="" disabled selected>Select event</option>
-                        <option value="Citizen Science Project">Citizen Science Project</option>
-                        <option value="BRCC Mission">BRCC Mission</option>
-                        <option value="Binational Expeditions">Binational Expeditions</option>
-                        <option value="Paleontological Mitigation Services">Paleontological Mitigation Services</option>
-                        <option value="Minerals">Minerals</option>
-                        <option value="San Diego">San Diego</option>
-                    </select><span class="error"><?php echo " " . $eventErr;?></span>
-                    <br /><br />
-                    
-                    <!-- Total field -->
-                    <p id="total">Total Number of Attendees: <?php echo $total; ?></p>
-                    
-                    <!-- Old Total Field
-                    <span class="required">*</span>
-                    <input type="total" name="total" id="total" size="40"  maxlength="2" value="<?php echo $total; ?>" placeholder="Total number of attendees (20 person limit)"><span class="error"><?php echo " " . $totalErr;?></span>
-                    <br /><br />
-                    -->
-                     
-                    <!-- Under 5 field -->
-                    <span class="required">&#9;*</span>
-                    <input type="under5" name="under5" id="under5" size="35"  maxlength="2" value="<?php echo $under5; ?>" placeholder="Number of attendees under 5" oninput="underFiveChange(this.value)"><span class="error"><?php echo " " . $under5Err;?></span>
-                    <br /><br />
-                    
-                    <!-- 5 to 12 field -->
-                    <span class="required">&#9;*</span>
-                    <input type="between5and12" name="between5and12" id="between5and12" size="35"  maxlength="2" value="<?php echo $between5and12; ?>" placeholder="Number of attendees between 5 and 12" oninput="fiveToTwelveChange(this.value)"><span class="error"><?php echo " " . $between5and12Err;?></span>
-                    <br /><br />
-                    
-                    <!-- 13 to 17 field -->
-                    <span class="required">&#9;*</span>
-                    <input type="between13and17" name="between13and17" id="between13and17" size="35"  maxlength="2" value="<?php echo $between13and17; ?>" placeholder="Number of attendees between 13 and 17" oninput="thirteenToSeventeenChange(this.value)"><span class="error"><?php echo " " . $between13and17Err;?></span>
-                    <br /><br />
-                    
-                    <!-- Over 17 field -->
-                    <span class="required">&#9;*</span>
-                    <input type="over17" name="over17" id="over17" size="35"  maxlength="2" value="<?php echo $over17; ?>" placeholder="Number of attendees between over 17" oninput="overSeventeenChange(this.value)"><span class="error"><?php echo " " . $over17Err;?></span>
-                    <br /><br />
-                    
-                    <!-- Newsletter Field -->
-                    <span class="optional">&#160;</span>
-                    <input type="checkbox" name="newsletter2" id="newsletter2" value="Signed up" checked> <label for="newsletter2">Sign up for our newsletter</label>
-                    <br /><br />
-                    
-                    <!-- Other Field -->
-                    <span class="optional">&#160;</span>
-                    <textarea name="other" id="other" rows="5" cols="40" value="<?php echo $other; ?>" placeholder="What other events would you like to see offered?"></textarea><span class="error"><?php echo " " . $otherErr;?></span>
-                    <br /><br />
+                        <!-- Last name field -->
+                        <tr>
+                            <td>Last Name:<span class="required">*</span></td>
+                            <td><input type="text" name="lastname" id="lastname" size="40"  maxlength="60" value="<?php echo $lastname; ?>"><span class="error"><?php echo " " . $lastnameErr;?></span>
+                            </td>
+                        </tr>
                         
+                        <!-- Email field -->
+                        <tr>
+                            <td>Email:<span class="required">*</span></td>
+                            <td><input type="email" name="email" id="email" size="40"  maxlength="60" value="<?php echo $email; ?>"><span class="error"><?php echo " " . $emailErr;?></span>
+                            </td>
+                        </tr>
+                        
+                        <!-- Address1 field -->
+                        <tr>
+                            <td>Address Line 1:<span class="optional">&#160;</span></td>
+                            <td><input type="address1" name="address1" id="address1" size="40"  maxlength="60" value="<?php echo $address1; ?>"><span class="error"><?php echo " " . $address1Err;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- Address2 field -->
+                        <tr>
+                            <td>Address Line 2:<span class="optional">&#160;</span></td>
+                            <td><input type="address2" name="address2" id="address2" size="40"  maxlength="60" value="<?php echo $address2; ?>"><span class="error"><?php echo " " . $address2Err;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- City, State, Zip field -->
+                        <tr>
+                            <td>City, State, Zip:<span class="optional">&#160;</span></td>
+                            <td><input type="city" name="city" id="city" size="26"  maxlength="30" value="<?php echo $city; ?>"><span class="error"><?php echo " " . $cityErr;?></span>
+                                <input type="state" name="state" id="state" size="4"  maxlength="2" value="<?php echo $state; ?>"><span class="error"><?php echo " " . $stateErr;?></span>
+                                <input type="zip" name="zip" id="zip" size="5"  maxlength="5" value="<?php echo $zip; ?>"><span class="error"><?php echo " " . $zipErr;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- Country field -->
+                        <tr>
+                            <td>Country:<span class="optional">&#160;</span></td>
+                            <td><input type="country" name="country" id="country" size="40"  maxlength="60" value="<?php echo $country; ?>"><span class="error"><?php echo " " . $countryErr;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- Phone field -->
+                        <tr>
+                            <td>Phone Number:<span class="optional">&#160;</span></td>
+                            <td><input type="phone" name="phone" id="phone" size="40"  maxlength="60" value="<?php echo $phone; ?>" placeholder="ex. 000-000-0000"><span class="error"><?php echo " " . $phoneErr;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- Event Field -->
+                        <tr>
+                            <td>Event:<span class="required">*</span></td>
+                            <td>
+                                <select name="event" id="event">
+                                    <option value="" disabled selected>Select event</option>
+                                    <option value="Citizen Science Project">Citizen Science Project</option>
+                                    <option value="BRCC Mission">BRCC Mission</option>
+                                    <option value="Binational Expeditions">Binational Expeditions</option>
+                                    <option value="Paleontological Mitigation Services">Paleontological Mitigation Services</option>
+                                    <option value="Minerals">Minerals</option>
+                                    <option value="San Diego">San Diego</option>
+                                </select><span class="error"><?php echo " " . $eventErr;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- Total field -->
+                        <tr>
+                            <td>Total number of attendees:<span class="required">*</span></td>
+                            <td><input type="total" name="total" id="total" size="40"  maxlength="2" value="<?php echo $total; ?>" placeholder="(20 person limit)"><span class="error"><?php echo " " . $totalErr;?></span>
+                                </td>
+                        </tr>
+                                
+                        <!-- Under 5 field -->
+                        <tr>
+                            <td>Number of attendees<br />under 5:<span class="required">*</span></td>
+                            <td><input type="under5" name="under5" id="under5" size="35"  maxlength="2" value="<?php echo $under5; ?>"><span class="error"><?php echo " " . $under5Err;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- 5 to 12 field -->
+                        <tr>
+                            <td>Number of attendees<br />between 5 and 12:<span class="required">*</span></td>
+                            <td><input type="between5and12" name="between5and12" id="between5and12" size="35"  maxlength="2" value="<?php echo $between5and12; ?>"><span class="error"><?php echo " " . $between5and12Err;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- 13 to 17 field -->
+                        <tr>
+                            <td>Number of attendees<br />between 13 and 17:<span class="required">*</span></td>
+                            <td><input type="between13and17" name="between13and17" id="between13and17" size="35"  maxlength="2" value="<?php echo $between13and17; ?>"><span class="error"><?php echo " " . $between13and17Err;?></span>
+                            </td>
+                        </tr>
+                                
+                        <!-- Over 17 field -->
+                        <tr>
+                            <td>Number of attendees<br />over 17:<span class="required">*</span></td>
+                            <td><input type="over17" name="over17" id="over17" size="35"  maxlength="2" value="<?php echo $over17; ?>"><span class="error"><?php echo " " . $over17Err;?></span>
+                            </td>
+                        </tr>
+                                    
+                        <!-- Newsletter Field -->
+                        <tr>
+                            <td>Newsletter:<span class="optional"></span></td>
+                            <td><input type="checkbox" name="newsletter" id="newsletter" value="Signed up" checked> <label for="newsletter">Sign up for our newsletter</label>
+                            </td>
+                        </tr>
+                                    
+                        <!-- Other Field -->
+                        <tr>
+                            <td>What other events would<br />you like to see offered?<span class="optional">&#160;</span></td>
+                            <td><textarea name="other" id="other" rows="5" cols="40" value="<?php echo $other; ?>"></textarea><span class="error"><?php echo " " . $otherErr;?></span>
+                            </td>
+                        </tr>
+                    </table>
+                    <br />
                     <input type="submit" value="Submit">
                     <input type="reset"><br /><br />
-                </form>
-                <!-- </article>
-            </section> -->
+                    </form>
+                </article>
+            </section>
         </div>
 
         <!-- footer -->
